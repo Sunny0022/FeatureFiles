f983e0951d0f95085711f1d7b6a6b76838546626
Sunny0022 <Sunny0022@users.noreply.github.com>
2020-04-30 18:37:27 +0530

    Create Google.rb

---
+++ Google.rb
@@ -0,0 +1,13 @@
+require 'selenium-webdriver'
+
+Given (/^that I have gone to the Google page$/) do
+  @driver = Selenium::WebDriver.for :firefox
+  @driver.navigate.to 'http://www.google.com'
+end
+
+When (/^I add "(.*)" to the search box$/) do |item|
+  @driver.find_element(:name, 'q').send_keys(item)
+end
+Then("I close the browser") do
+  @driver.quit
+end
