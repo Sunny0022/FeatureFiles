22d03661f45d45f6138804a34948bae37c229988
Sunny0022 <Sunny0022@users.noreply.github.com>
2020-05-15 20:02:12 +0530

    Update and rename Google.rb to psignup.rb

---
+++ psignup.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+Given(/^i am on page$/) do
+  @driver = Selenium::WebDriver.for :firefox
+  @driver.get 'http://uday.domain4now.com/'
+end
+
+And(/^i click sign up link$/) do
+  @driver.find_element(:xpath, '/html/body/nav/div[2]/ul/li[2]/a').click
+  @driver.manage.timeouts.implicit_wait = 30
+end
+
+When(/^i fill the form$/) do
+  @driver.find_element(:id, 'user_name').send_keys('automate12')
+  @driver.find_element(:id, 'user_email').send_keys('automate12@domain.com')
+  @driver.find_element(:id, 'user_password').send_keys('Password123,.')
+  @driver.find_element(:id, 'user_password_confirmation').send_keys('Password123,.')
+  @driver.find_element(:xpath, '/html/body/div/div/div/div/div[2]/form/div/div[2]/input').click
+  @driver.manage.timeouts.implicit_wait = 30
+end
+
+Then(/^i display result$/) do
+  sleep 5
+  @driver.quit
+  puts 'new user created'
+end
